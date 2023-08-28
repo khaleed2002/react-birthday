7c6bd88114e8ee53f18531442886053f332b0c46
khaleed2002 <zekomadred159@gmail.com>
2023-08-28 22:10:45 +0300

    add button to remove all

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,27 @@ import Person from './components/Person'
 import data from './data'
 const App = () => {
   const [dummyData, setData] = useState(data)
-  
+  const handleClick = () => {
+    setData([])
+  }
   return (
-    <div className="main">
+    <div
+      className="container"
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <div className="container">
         <h3>{dummyData.length} Close Friends</h3>
         {dummyData.map((friend) => {
           return <Person {...friend} key={friend.id} />
         })}
       </div>
+      <button className="btn" onClick={handleClick}>
+        Remove All
+      </button>
     </div>
   )
 }
