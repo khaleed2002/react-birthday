90bc1269d5bd2c92b23de955e4cdbca1b477337e
khaleed2002 <zekomadred159@gmail.com>
2023-08-28 21:29:11 +0300

    create Person component

---
+++ src/components/Person.jsx
@@ -0,0 +1,14 @@
+const Person = (props) => {
+    const { name, age, img } = props;
+
+    return (
+        <div className="person">
+            <img src={img} />
+            <div>
+                <h4>{name}</h4>
+                <p>{age} years</p>
+            </div>
+        </div>
+    )
+}
+export default Person
