05d33d2af5d092b484f6d2582bc3e1524de77604
khaleed2002 <zekomadred159@gmail.com>
2023-08-28 22:05:15 +0300

    return person

--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,8 +1,5 @@
-import { useState } from 'react'
-import { AiOutlineHeart } from 'react-icons/ai'
 const Person = (props) => {
   const { name, age, img } = props
-  const [color, setColor] = useState('')
   return (
     <div className="person">
       <img src={img} />
@@ -10,9 +7,6 @@ const Person = (props) => {
         <h4>{name}</h4>
         <p>{age} years</p>
       </div>
-      <button onClick={() => setColor('red')}>
-        <AiOutlineHeart baseProfile="red" width="20px" height="20px" />
-      </button>
     </div>
   )
 }
